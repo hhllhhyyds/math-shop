e997f3db999db2e9cd44c0888dd91a0783937757
hhllhhyyds <hhllhhyyds@sina.com>
2024-03-15 17:42:02 +0800

    Simplify code with constant generic

--- crates/fft-shop/src/recursive_radix2_fft.rs
+++ crates/fft-shop/src/recursive_radix2_fft.rs
@@ -1,34 +1,20 @@
 use crate::common::{n_is_power_of_2, unit_complex};
 use refined_float::{Complex, FloatTraitsForComplex};
 
-fn fft_local<F: FloatTraitsForComplex>(data: &mut [Complex<F>], temp: &mut [Complex<F>]) {
+fn fft_local<F: FloatTraitsForComplex, const SIGN: i64>(
+    data: &mut [Complex<F>],
+    temp: &mut [Complex<F>],
+) {
     let n = data.len();
     if n > 1 {
         (0..(n / 2)).for_each(|j| {
             temp[j] = data[j] + data[j + n / 2];
-            temp[j + n / 2] = (data[j] - data[j + n / 2]) * unit_complex(n as u64, -(j as i64));
+            temp[j + n / 2] =
+                (data[j] - data[j + n / 2]) * unit_complex(n as u64, SIGN * (j as i64));
         });
 
-        fft_local(&mut temp[..(n / 2)], &mut data[..(n / 2)]);
-        fft_local(&mut temp[(n / 2)..], &mut data[(n / 2)..]);
-
-        (0..(n / 2)).for_each(|j| {
-            data[2 * j] = temp[j];
-            data[2 * j + 1] = temp[j + (n / 2)];
-        });
-    }
-}
-
-fn ifft_local<F: FloatTraitsForComplex>(data: &mut [Complex<F>], temp: &mut [Complex<F>]) {
-    let n = data.len();
-    if n > 1 {
-        (0..(n / 2)).for_each(|j| {
-            temp[j] = data[j] + data[j + n / 2];
-            temp[j + n / 2] = (data[j] - data[j + n / 2]) * unit_complex(n as u64, j as i64);
-        });
-
-        ifft_local(&mut temp[..(n / 2)], &mut data[..(n / 2)]);
-        ifft_local(&mut temp[(n / 2)..], &mut data[(n / 2)..]);
+        fft_local::<F, SIGN>(&mut temp[..(n / 2)], &mut data[..(n / 2)]);
+        fft_local::<F, SIGN>(&mut temp[(n / 2)..], &mut data[(n / 2)..]);
 
         (0..(n / 2)).for_each(|j| {
             data[2 * j] = temp[j];
@@ -50,7 +36,7 @@ pub fn fft<F: FloatTraitsForComplex>(data: &[Complex<F>]) -> Vec<Complex<F>> {
         let mut data = data.to_vec();
         let mut temp = vec![Complex::new(F::ZERO, F::ZERO); n];
 
-        fft_local(&mut data, &mut temp);
+        fft_local::<F, -1>(&mut data, &mut temp);
 
         data
     }
@@ -69,7 +55,7 @@ pub fn ifft<F: FloatTraitsForComplex>(data: &[Complex<F>]) -> Vec<Complex<F>> {
         let mut data = data.to_vec();
         let mut temp = vec![Complex::new(F::ZERO, F::ZERO); n];
 
-        ifft_local(&mut data, &mut temp);
+        fft_local::<F, 1>(&mut data, &mut temp);
 
         data.iter_mut()
             .for_each(|x| *x = x.scale(F::ONE / F::from_f64(n as f64)));
